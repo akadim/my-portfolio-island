a926ac090e7de174f05852d7e2807182fe018b09
Ahmed KADIM <kadim.ahmed.web@gmail.com>
2024-10-17 17:45:29 +0100

    Adding the About Section

---
+++ src/data/index.ts
@@ -0,0 +1,244 @@
+import { autorabit, bisim, ciklum, mostaql } from "../assets/images";
+import {
+  car,
+  contact,
+  css,
+  estate,
+  express,
+  git,
+  github,
+  html,
+  javascript,
+  linkedin,
+  mongodb,
+  angular,
+  mui,
+  nextjs,
+  nodejs,
+  java,
+  springboot,
+  pricewise,
+  react,
+  redux,
+  sass,
+  snapgram,
+  summiz,
+  tailwindcss,
+  threads,
+  typescript,
+} from "../assets/icons";
+
+export const skills = [
+  {
+    imageUrl: css,
+    name: "CSS",
+    type: "Frontend",
+  },
+  {
+    imageUrl: express,
+    name: "Express",
+    type: "Backend",
+  },
+  {
+    imageUrl: git,
+    name: "Git",
+    type: "Version Control",
+  },
+  {
+    imageUrl: github,
+    name: "GitHub",
+    type: "Version Control",
+  },
+  {
+    imageUrl: html,
+    name: "HTML",
+    type: "Frontend",
+  },
+  {
+    imageUrl: javascript,
+    name: "JavaScript",
+    type: "Frontend",
+  },
+  {
+    imageUrl: mongodb,
+    name: "MongoDB",
+    type: "Database",
+  },
+  {
+    imageUrl: angular,
+    name: "Angular",
+    type: "Frontend",
+  },
+  {
+    imageUrl: react,
+    name: "React",
+    type: "Frontend",
+  },
+  {
+    imageUrl: nextjs,
+    name: "Next.js",
+    type: "Frontend",
+  },
+  {
+    imageUrl: mui,
+    name: "Material-UI",
+    type: "Frontend",
+  },
+  {
+    imageUrl: nodejs,
+    name: "Node.js",
+    type: "Backend",
+  },
+  {
+    imageUrl: java,
+    name: "Java",
+    type: "Backend",
+  },
+  {
+    imageUrl: springboot,
+    name: "Spring Boot",
+    type: "Backend",
+  },
+  {
+    imageUrl: redux,
+    name: "Redux",
+    type: "State Management",
+  },
+  {
+    imageUrl: sass,
+    name: "Sass",
+    type: "Frontend",
+  },
+  {
+    imageUrl: tailwindcss,
+    name: "Tailwind CSS",
+    type: "Frontend",
+  },
+  {
+    imageUrl: typescript,
+    name: "TypeScript",
+    type: "Frontend",
+  },
+];
+
+export const experiences = [
+  {
+    title: "Full Stack Developer",
+    company_name: "AutoRabit",
+    icon: autorabit,
+    iconBg: "#accbe1",
+    date: "January 2023 - February 2024",
+    points: [
+      "Developing and maintaining web applications using React.js and Java Spring Boot.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Frontend Developer / UI Progammer",
+    company_name: "Bohemia Interactive Simuations",
+    icon: bisim,
+    iconBg: "#fbc3bc",
+    date: "February 2022 - Feb 2022",
+    points: [
+      "Developing and maintaining the UI parts of a simulation game Using Angular.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Full Stack Developer",
+    company_name: "Ciklum (CN Group Formerly) ",
+    icon: ciklum,
+    iconBg: "#b7e4c7",
+    date: "Jan 2018 - Jan 2020",
+    points: [
+      "Developing and maintaining web applications using React.js and other Java/Spring Boot.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Full stack Developer",
+    company_name: "Mostaql",
+    icon: mostaql,
+    iconBg: "#a2d2ff",
+    date: "Jan 2012 - Jan 2018",
+    points: [
+      "Developing and maintaining Freelance web applications using React.js, Angular, Java/Spring and PHP/Laravel.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+    ],
+  },
+];
+
+export const socialLinks = [
+  {
+    name: "Contact",
+    iconUrl: contact,
+    link: "/contact",
+  },
+  {
+    name: "GitHub",
+    iconUrl: github,
+    link: "https://github.com/YourGitHubUsername",
+  },
+  {
+    name: "LinkedIn",
+    iconUrl: linkedin,
+    link: "https://www.linkedin.com/in/YourLinkedInUsername",
+  },
+];
+
+export const projects = [
+  {
+    iconUrl: pricewise,
+    theme: "btn-back-red",
+    name: "Amazon Price Tracker",
+    description:
+      "Developed a web application that tracks and notifies users of price changes for products on Amazon, helping users find the best deals.",
+    link: "https://github.com/adrianhajdin/pricewise",
+  },
+  {
+    iconUrl: threads,
+    theme: "btn-back-green",
+    name: "Full Stack Threads Clone",
+    description:
+      'Created a full-stack replica of the popular discussion platform "Threads," enabling users to post and engage in threaded conversations.',
+    link: "https://github.com/adrianhajdin/threads",
+  },
+  {
+    iconUrl: car,
+    theme: "btn-back-blue",
+    name: "Car Finding App",
+    description:
+      "Designed and built a mobile app for finding and comparing cars on the market, streamlining the car-buying process.",
+    link: "https://github.com/adrianhajdin/project_next13_car_showcase",
+  },
+  {
+    iconUrl: snapgram,
+    theme: "btn-back-pink",
+    name: "Full Stack Instagram Clone",
+    description:
+      "Built a complete clone of Instagram, allowing users to share photos and connect with friends in a familiar social media environment.",
+    link: "https://github.com/adrianhajdin/social_media_app",
+  },
+  {
+    iconUrl: estate,
+    theme: "btn-back-black",
+    name: "Real-Estate Application",
+    description:
+      "Developed a web application for real estate listings, facilitating property searches and connecting buyers with sellers.",
+    link: "https://github.com/adrianhajdin/projects_realestate",
+  },
+  {
+    iconUrl: summiz,
+    theme: "btn-back-yellow",
+    name: "AI Summarizer Application",
+    description:
+      "App that leverages AI to automatically generate concise & informative summaries from lengthy text content, or blogs.",
+    link: "https://github.com/adrianhajdin/project_ai_summarizer",
+  },
+];
